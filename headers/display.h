16e916a78231e0822f596b1257263b24ac9ad99c
Michael Jenkins <MAJ4k@icloud.com>
2020-07-24 16:25:21 -0400

    following the benny box tutorial trying to figure out how I want to implement open GL

---
+++ headers/display.h
@@ -0,0 +1,66 @@
+#pragma once
+#include "../common.h"
+
+class Display
+{
+private:
+    SDL_Window* winptr;
+    SDL_GLContext glcontx;
+    bool isClosed;
+public:
+    Display(unsigned short width, unsigned short height, const char* title);
+    ~Display();
+
+    void update();
+    bool IsClosed(){return isClosed;}
+};
+
+Display::Display(unsigned short width,unsigned short height,const char* title)
+{
+    SDL_Init(SDL_INIT_EVERYTHING);
+
+    SDL_GL_SetAttribute(SDL_GL_RED_SIZE, 8);
+    SDL_GL_SetAttribute(SDL_GL_GREEN_SIZE, 8);
+    SDL_GL_SetAttribute(SDL_GL_BLUE_SIZE, 8);
+    SDL_GL_SetAttribute(SDL_GL_ALPHA_SIZE, 8);
+    SDL_GL_SetAttribute(SDL_GL_BUFFER_SIZE, 32);
+    SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+
+    winptr = SDL_CreateWindow(title, 
+    SDL_WINDOWPOS_CENTERED,
+    SDL_WINDOWPOS_CENTERED,
+    width,
+    height,
+    SDL_WINDOW_OPENGL);
+
+    glcontx = SDL_GL_CreateContext(winptr);
+
+    GLenum status = glewInit();
+
+    if (status != GLEW_OK)
+    {
+        std::cerr << "Glew failed initialization\n";
+    }
+
+    isClosed = false;
+}
+
+Display::~Display()
+{
+    SDL_GL_DeleteContext(glcontx);
+    SDL_DestroyWindow(winptr);
+    SDL_Quit();
+}
+
+void Display::update()
+{
+    SDL_GL_SwapWindow(winptr);
+
+    SDL_Event e;
+    
+    while(SDL_PollEvent(&e))
+    {
+        if(e.type == SDL_QUIT)
+            isClosed = true;
+    }
+}
--- main.cpp
+++ main.cpp
@@ -1,6 +1,53 @@
-#include <iostream>
+#include "common.h"
+#include "headers/display.h"
 
-int main(){
-    std::cout << "YATTA\n";
+int main(int argc,char *argv[])
+{
+    Display test (800,600,"YATTA");
+
+    while (!test.IsClosed())
+    {
+        glClearColor(0.0f,0.15f,0.3f,1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        test.update();
+    }
     return 0;
 }
+
+/*  // SDL init
+    if (SDL_Init(SDL_INIT_NOPARACHUTE) && SDL_Init(SDL_INIT_EVERYTHING) != 0)
+    {
+        SDL_Log("Unable to initialise SDL: %s", SDL_GetError());
+        return -1;
+    }
+    else SDL_Log("SDL Initialised");
+    SDL_Window* window = SDL_CreateWindow(
+        "Test",
+        SDL_WINDOWPOS_CENTERED,
+        SDL_WINDOWPOS_CENTERED,
+        640,
+        480,
+        SDL_WINDOW_OPENGL
+    );
+
+    // Gen an OpenGL context and map to window instance
+    SDL_GLContext glContext = SDL_GL_CreateContext(window);
+
+    bool isRunning = true;
+    while (isRunning)
+    {
+        // Ready buffers, and switch
+        SDL_GL_SwapWindow(window);
+
+    }
+
+
+    // Clean up
+    SDL_GL_DeleteContext(glContext);
+    SDL_DestroyWindow(window);
+
+
+    std::cout << "YATTA\n";
+    return 0;
+} */
